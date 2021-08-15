ffaa1d8350a6bb4f482118a6a37acd35b345ef2a
daichitt <gene.sixteen.000925@outlook.jp>
2021-08-15 21:45:41 +0900

    コンポーネント

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -2,4 +2,7 @@
 <x-slot name="header">ヘッダー2</x-slot>
 
 コンポーネントテスト2
+<x-test-class-base classBaseMessage="メッセージです" />
+<x-test-class-base classBaseMessage="メッセージです", defaultMessage="デフォルトメッセージ" />
+
 </x-tests.app>
