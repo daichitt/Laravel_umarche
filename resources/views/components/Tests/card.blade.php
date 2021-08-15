12653fec1630376d899fc88c1b0d65eb4c93fe57
daichitt <gene.sixteen.000925@outlook.jp>
2021-08-15 21:07:28 +0900

    属性BAG

--- resources/views/components/Tests/card.blade.php
+++ resources/views/components/Tests/card.blade.php
@@ -1,11 +1,13 @@
 <!-- 値がない時にエラーが出ないように初期値を設定 -->
-props([
+@props([
     'title',
     'message' => '初期値です',
     'content' => '本文初期値です'
 ])
 
-<div class="border-2 shadow-md w-1/4 p-2">
+<div {{ $attributes->merge([
+    'class' => 'border-2 shadow-md w-1/4 p-2'
+    ]) }} >
 <div>{{ $title }}</div>
 <div>画像</div>
 
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -6,4 +6,5 @@
 
 <x-tests.card title="タイトル" content="コンテント" :message="$message" />
 <x-tests.card title="タイトル2" />
+<x-tests.card title="CSS変更" class="bg-red-300" />
 </x-tests.app>
