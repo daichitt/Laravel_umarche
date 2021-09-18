234a9187a9064184080f15b38376d2f9f7db197d
daichitt <gene.sixteen.000925@outlook.jp>
2021-09-18 17:38:23 +0900

    image初期設定

---
+++ database/migrations/2021_09_18_165405_create_images_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            // 外部キー制約
+            $table->foreignId('owner_id')
+            ->constrained()
+            ->upDate('cascade')
+            ->onDelete('cascade');
+            $table->string('filename');
+            $table->string('title')->nullable();
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
--- routes/owner.php
+++ routes/owner.php
@@ -7,9 +7,11 @@ use App\Http\Controllers\Owner\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Owner\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Owner\Auth\EmailVerificationPromptController;
 use App\Http\Controllers\Owner\Auth\NewPasswordController;
-use App\Http\Controllers\Owner\Auth\PassworOwnerdResetLinkController;
+use App\Http\Controllers\Owner\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Owner\Auth\RegisteredUserController;
 use App\Http\Controllers\Owner\ShopController;
+use App\Http\Controllers\Owner\ImageController;
+
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
 
 
@@ -35,6 +37,9 @@ middleware('auth:owners')->group(function(){
     Route::get('edit/{shop}', [ShopController::class, 'edit'])->name('shops.edit');
     Route::post('update/{shop}', [ShopController::class, 'update'])->name('shops.update');
 });
+
+Route::resource('images', ImageController::class)
+->middleware('auth:owners')->except(['show']);
 
 Route::get('/dashboard', function () {
     return view('owner.dashboard');
