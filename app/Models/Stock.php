3d6cfdca8639d257726cc90335c59a07a1960c72
daichitt <gene.sixteen.000925@outlook.jp>
2021-09-22 22:44:26 +0900

    stock

---
+++ app/Models/Stock.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use App\Models\Product;
+
+class Stock extends Model
+{
+    use HasFactory;
+
+    // テーブル名をt_stocksに
+    protected $table = 't_stocks';
+
+    // public function image()
+    // {
+    //     return $this->belongsTo(Product::class);
+    // }
+}
